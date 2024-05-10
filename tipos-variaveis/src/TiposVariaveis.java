c27848f6be3becc3b146bb3082b33775bef83695
lfsp7 <lfsalamanca07@gmail.com>
2024-05-10 10:40:30 -0300

    Projetor-Smart-Tv-Java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Luis Fernando";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // após apresentado um erro é possível adicionar um recurso de correção chamado "Cast"
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
